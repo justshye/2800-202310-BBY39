e2ebf3001442ab25f32ad41b331622c53a55096e
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2023-05-09 11:05:07 -0700

    add mongodb information to index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
-const url = require('url');
+// const url = require('url');
 const saltRounds = 12;
 
 const port = process.env.PORT || 4420;
@@ -25,12 +25,53 @@ const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_database = process.env.MONGODB_DATABASE;
 /* secret information */
 
+
+var {database} = include("./databaseConnection.js");
+const userCollection = database.db(mongodb_database).collection("users");
+
+var mongoStore = MongoStore.create({
+    mongoUrl:
+    `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true&w=majority`,
+    crypto: {
+        secret: mongodb_session_secret
+    }
+});
+
+app.use(session({
+    secret: node_session_secret,
+        store: mongoStore, //default is memory, but we want to use mongo
+        saveUninitialized: false,
+        resave: false,
+}
+));
+
+function isValidSession(req) {
+    return req.session.authenticated;
+}
+
+function sessionValidation(req, res, next) {
+    if (isValidSession(req)) {
+        next();
+    } 
+    else {
+        res.redirect('/login');
+    }
+}
+
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: false}));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
+// app.get('/', (req, res) => {
+// 	res.
+// });
+
+app.get("*", (req, res) => {
+    res.status(404);
+    res.render("404");
+});
 app.listen(port, () => {
 	console.log(`Node application listening on port ${port}`);
 }); 
