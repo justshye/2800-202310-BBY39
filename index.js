76d707c7eb05d0a7d027258267ab8f725844d7d9
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2023-05-09 15:09:59 -0700

    fixed merge error from previous commit

--- index.js
+++ index.js
@@ -84,9 +84,132 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + "/public"));
 
-// app.get('/', (req, res) => {
-// 	res.
-// });
+app.get('/', (req, res) => {
+    res.render("homepage", {
+        user: req.session.username,
+        authenticated: req.session.authenticated
+    });
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/loginSubmit", async (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().max(20).required(),
+  });
+
+  const validationResult = schema.validate({ email, password });
+
+  if (validationResult.error != null) {
+    res.render("login-submit", {
+      validationError: true,
+      validationMessage: validationResult.error.message,
+    });
+    return;
+  }
+
+  const result = await userCollection
+    .find({ email: email })
+    .project({ email: 1, username: 1, password: 1, user_type: 1, _id: 1 })
+    .toArray();
+
+  if (result.length != 1) {
+    res.render("login-submit", {
+      validationError: false,
+      userFound: false,
+    });
+    return;
+  }
+
+  if (await bcrypt.compare(password, result[0].password)) {
+    req.session.authenticated = true;
+    req.session.email = email;
+    req.session.cookie.maxAge = expireTime;
+    req.session.username = result[0].username;
+    req.session.user_type = result[0].user_type;
+    req.session.save(() => {
+      res.redirect("/");
+    });
+    return;
+  } else {
+    res.render("login-submit", {
+      validationError: false,
+      userFound: true,
+      correctPassword: false,
+    });
+    return;
+  }
+});
+
+app.post("/signupSubmit", async (req, res) => {
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+
+  const schema = Joi.object({
+    username: Joi.string().alphanum().max(20).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().max(20).required(),
+  });
+
+  const validationResult = schema.validate({ username, email, password });
+  if (validationResult.error != null) {
+    res.render("signup-submit", {
+      validationMessage: validationResult.error.message,
+    });
+    return;
+  }
+
+  const userAlreadyExists = await userCollection.findOne({
+    $or: [{ username: username }, { email: email }],
+  });
+
+  if (userAlreadyExists) {
+    if (userAlreadyExists.username == username) {
+      res.render("signup-submit", {
+        validationMessage: "Username is already taken!",
+      });
+    } else if (userAlreadyExists.email == email) {
+      res.render("signup-submit", {
+        validationMessage: "Email is already in use!",
+      });
+    }
+
+    return;
+  }
+
+  var hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  await userCollection.insertOne({
+    username: username,
+    email: email,
+    password: hashedPassword,
+    user_type: "user",
+  });
+
+  req.session.authenticated = true;
+  req.session.email = email;
+  req.session.cookie.maxAge = expireTime;
+  req.session.username = username;
+  req.session.save(() => {
+    res.redirect("/");
+  });
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
 
 app.get('/openai', async (req, res) => {
 	try {
