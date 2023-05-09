1a245a111a91ac67353083c291075ed276d11819
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2023-05-09 14:52:51 -0700

    successfully connected code to gpt 3.5

--- index.js
+++ index.js
@@ -39,25 +39,26 @@ var mongoStore = MongoStore.create({
 });
 
 const configuration = new Configuration({
+	organization: process.env.OPENAI_ORGANIZATION_ID,
 	apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-async function getOpenAIResponse() {
-	const response = await openai.createCompletion({
-	  model: "text-davinci-003",
-	  prompt: "This is a test",
-	  temperature: 0,
-	  max_tokens: 1,
-	  top_p: 1.0,
-	  frequency_penalty: 0.0,
-	  presence_penalty: 0.0,
+async function getOpenAIResponse(prompt) {
+	const completion = await openai.createChatCompletion({
+		model: "gpt-3.5-turbo",
+		messages: [
+			{role: "user", content: prompt},
+		],
+		max_tokens: 10,
+		temperature: 0,
+		top_p: 1,
+		frequency_penalty: 0,
+		presence_penalty: 0,
 	});
-  
-	const generatedMessage = response.data.choices[0].text;
-	console.log(generatedMessage); // Log the generated message
-	return response;
-}
+	console.log(completion.data.choices[0].message);
+	return completion.data.choices[0].message.content;
+  }
 
 app.use(session({
     secret: node_session_secret,
@@ -92,19 +93,21 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/openai', async (req, res) => {
 	try {
-	  const response = await getOpenAIResponse();
-	  const prompts = response.data.choices.map(choice => choice.prompt);
-	  res.render('openai', { prompts });
+	  const prompt = req.query.prompt || ''; // Get the prompt from the query parameter or use an empty string as the default
+	  let response;
+	  if (prompt != '' ) {
+		 response = await getOpenAIResponse(prompt);
+	  } 
+	  else {
+		 response = '';
+	  }
+	  res.render('openai', { prompt, generatedMessage: response });
 	} catch (error) {
 	  console.error(error);
 	  res.status(500).send('Internal Server Error');
 	}
   });
-
   
-  
-  
-
 app.get("*", (req, res) => {
     res.status(404);
     res.render("404");
