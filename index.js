3931a678b2f444b4dff5dd84277e7e72bc6854d8
Saerek <sampark1080@gmail.com>
2023-05-17 14:03:53 -0700

    Add working watchlist function and page

--- index.js
+++ index.js
@@ -547,6 +547,23 @@ app.get("/random-movie", async (req, res) => {
   }
 });
 
+app.get("/display-watchlist", async (req, res) => {
+  try {
+    const filter = { username: req.session.username };
+    const result = await userCollection.findOne(filter);
+
+    if (result) {
+      const watchlist = result.watchlist;
+      res.json(watchlist);
+    } else {
+      console.log("User not found");
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    console.error("Error retrieving watchlist:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 // const { ObjectId } = require('mongodb');
 
