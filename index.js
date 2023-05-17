d1d14b9fad02228393aa1f6f4d1c477e962b4789
CyrusVillaruz <cyrusvillaruz1@gmail.com>
2023-05-16 21:09:34 -0700

    Duplicates Movie Adding Logic To "not-interested" movies

--- index.js
+++ index.js
@@ -561,6 +561,45 @@ app.get('/add-to-interested', async (req, res) => {
   }
 });
 
+app.get('/add-to-not-interested', async (req, res) => {
+  try {
+    const userId = req.session.userId;
+
+    if (!userId) {
+      throw new Error("User not authenticated");
+    }
+
+    const movieId = req.query.movieId;
+    const movie = await movieCollection.findOne({ _id: new ObjectId(movieId) });
+
+    if (!movie) {
+      throw new Error("Movie not found");
+    }
+
+    const newMovie = {
+      Release_Date: movie["Release_Date"],
+      Title: movie["Title"],
+      Overview: movie["Overview"],
+      Popularity: movie["Popularity"],
+      Vote_Count: movie["Vote_Count"],
+      Vote_Average: movie["Vote_Average"],
+      Original_Language: movie["Original_Language"],
+      Genre: movie["Genre"],
+      Poster_Url: movie["Poster_Url"]
+    };
+
+    await userCollection.updateOne(
+      { _id: new ObjectId(userId) },
+      { $push: { uninterestingMovies: newMovie } }
+    );
+
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
+  }
+});
+
 app.get("*", (req, res) => {
   res.status(404);
   res.render("404");
