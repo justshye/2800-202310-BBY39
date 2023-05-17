2b88cfeb902c7290b1bcdd268007e088493e4d72
shye <113232835+justshye@users.noreply.github.com>
2023-05-17 11:35:51 -0700

    handles error fetching data and passes boolean to res.render

--- index.js
+++ index.js
@@ -444,14 +444,19 @@ app.get('/profile', async function (req, res) {
   } else {
     const username = req.session.username;
     try {
-      const user = await userCollection.findOne({ username: username }, { avatar: 1 });
-      res.render("profile", { user: username, email: req.session.email, avatar: user.avatar });
+      const user = await userCollection.findOne({ username: username }, { avatar: 1, moviesWatched: 1 });
+      const moviesWatched = user.moviesWatched ? user.moviesWatched.length : 0;
+      const movieMateUnlocked = moviesWatched >= 5;
+      res.render("profile", { user: username, email: req.session.email, avatar: user.avatar, moviesWatched, movieMateUnlocked });
     } catch (err) {
       console.error(err);
-      res.status(500).send('Error fetching user data');
+      const movieMateUnlocked = false; // Set movieMateUnlocked to false
+      res.render("profile", { user: username, email: req.session.email, avatar: user.avatar, moviesWatched: 0, movieMateUnlocked });
     }
   }
 });
+
+
 
 
 app.get('/friends', sessionValidation, async (req, res) => {
