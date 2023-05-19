a755a5605dbf180cca45fb40d74fefe78d056162
shye <113232835+justshye@users.noreply.github.com>
2023-05-19 00:56:59 -0700

    added more stat functionality

--- components/stats.js
+++ components/stats.js
@@ -9,14 +9,17 @@ async function stats(req, res) {
     const user = await userCollection.findOne({ username: username }, { moviesWatched: 1 });
     const watchlistSize = user.watchlist.length;
     let moviesWatched = 0;
+    let moviesPlanned = 0;
     for (const movie of user.watchlist) {
       if (movie.Watched === "Completed") {
         moviesWatched++;
+      } else if (movie.Watched === "Plan to Watch") {
+        moviesPlanned++;
       }
     }
     const watchHours = Math.floor((moviesWatched * 130.9) / 60); //average movie duration is 130.9 minutes
     const watchMinutes = Math.floor((moviesWatched * 130.9) % 60);
-    res.render("stats", { user: username, watchlistSize, moviesWatched, watchHours, watchMinutes, rejectedMovieCount: user.rejectedMovies.length });
+    res.render("stats", { user: username, watchlistSize, moviesWatched, watchHours, watchMinutes, moviesPlanned, rejectedMovieCount: user.rejectedMovies.length });
   }
 }
 
