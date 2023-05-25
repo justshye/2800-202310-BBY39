63c3fd8498d3588b3c5f90ab63ebe5ab724ee3c6
CyrusVillaruz <cyrusvillaruz1@gmail.com>
2023-05-25 16:29:23 -0700

    Updates image file path

--- setup/index.js
+++ setup/index.js
@@ -55,7 +55,7 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/../public"));
 
 let cachedMovies = null;
 
