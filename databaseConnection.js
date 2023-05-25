e45ea98b49bfbb969b7025c642a9b3d3235d9cc1
CyrusVillaruz <cyrusvillaruz1@gmail.com>
2023-05-25 13:16:59 -0700

    Adds GPT reference in databaseConnection

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
-const { MongoClient, ObjectId } = require("mongodb"); // Import MongoClient and ObjectId
+const { MongoClient, ObjectId } = require("mongodb"); // This line was updated by ChatGPT to include ObjectID
 
 const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true&w=majority`;
 var database = new MongoClient(atlasURI, {useNewUrlParser: true, useUnifiedTopology: true});
-module.exports = { database, ObjectId }; // Export MongoClient and ObjectId
+module.exports = { database, ObjectId }; // This line was updated by ChatGPT to include ObjectID
